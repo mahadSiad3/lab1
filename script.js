ad8373acc1777fc72c03b1e39cbb96b68f890779
mahadsiad3 <mahadsiad3@gmail.com>
2025-05-01 15:06:00 -0400

    updating info

---
+++ script.js
@@ -0,0 +1,48 @@
+// //Part 1: Refactoring Old Code
+
+// let str ="ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+// let row=[];
+// let cell="";
+// let table=[];
+// for (let i=0;i<str.length;i++){
+//     if(str[i]===","){
+//         row += " "
+//     }
+//     else if(str[i]==="\n"){
+//         console.log(row);
+//         row="";
+//     }
+//     else{
+//         row += str[i];
+//     }
+// }
+
+let varData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+let row = []
+let cell = ""
+let table = []
+let count=0;
+for (let i = 0; i <= varData.length; i++) {
+    if (varData[i] === ",") {
+        row.push(cell)
+      // console.log("cell:"+cell)
+         cell = ""
+        //console.log("====== before else if=============")
+       // console.log(row[0], row[1], row[2], row[3]);
+    } else if (varData[i] === "\n") {
+        row.push(cell)
+       // console.log("cell2:"+cell)
+         cell = ""
+         count++;
+       table.push(row)
+       table.reverse;
+        // console.log(row[0], row[1], row[2], row[3]);
+         row=[]
+    }
+    else {
+        cell += varData[i];
+    }
+    
+}
+console.log(table)
+console.log("Number of rows "+count)
